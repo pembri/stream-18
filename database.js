11ceca5b72994979e5b675d7b436b5deff0e532f
pembri <pembriahmad@gmail.com>
2026-05-04 23:46:51 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,1 @@
-// File ini akan diupdate otomatis oleh admin_post.html menggunakan GitHub API
-// Jangan ubah struktur variabel const videoDB secara manual agar sistem admin tidak error.
-
 const videoDB = [];
