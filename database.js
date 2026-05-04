9f810170754c4bdcbc26c6fe9f088e6aedf44c40
pembri <pembriahmad@gmail.com>
2026-05-04 23:50:02 +0700

    Add: Pijatan aki aki

--- database.js
+++ database.js
@@ -1,1 +1,10 @@
-const videoDB = [];
+const videoDB = [
+    {
+        "slug": "pijatan-aki-aki",
+        "title": "Pijatan aki aki",
+        "category": "pijat",
+        "type": "embed",
+        "content": "<iframe width=\"640\" height=\"360\" src=\"https://tv1.indoav.app/e/THP1tX8RQ90u\" frameborder=\"0\" allowfullscreen=\"true\"></iframe>",
+        "date": "2026-05-04T16:50:00.655Z"
+    }
+];
