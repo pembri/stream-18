f5ae1ad4b3e73f4ac93bddfc19f8c3fb6d37a933
pembri <pembriahmad@gmail.com>
2026-05-08 04:01:30 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,19 +1,4 @@
-/**
- * STREAM 18 - DATABASE VIDEO
- * File ini digunakan sebagai penyimpan data statis.
- * Jangan ubah struktur array objek di bawah ini secara manual 
- * jika tidak paham, gunakan panel Admin untuk menambah video.
- */
-
 const videoData = [
-    {
-        "id": 1715113800000,
-        "title": "Trailer Film Action Super Keren 2026",
-        "category": "Action",
-        "slug": "/content_video/Action/trailer-film-action-super-keren-2026",
-        "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
-        "date": "2026-05-08"
-    },
     {
         "id": 1715113850000,
         "title": "Kompilasi Komedi Bikin Ngakak",
