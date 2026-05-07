1cab0e6cbe87ffe89e8f6764c2be9dfa3ec221ec
pembri <pembriahmad@gmail.com>
2026-05-08 04:01:39 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const videoData = [
     {
-        "id": 1715113850000,
-        "title": "Kompilasi Komedi Bikin Ngakak",
-        "category": "Comedy",
-        "slug": "/content_video/Comedy/kompilasi-komedi-bikin-ngakak",
-        "thumbnail": "https://images.unsplash.com/photo-1527228113244-0b730591f893?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+        "id": 1715113800000,
+        "title": "Trailer Film Action Super Keren 2026",
+        "category": "Action",
+        "slug": "/content_video/Action/trailer-film-action-super-keren-2026",
+        "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
         "date": "2026-05-08"
     }
 ];
