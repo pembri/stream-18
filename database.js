d307243b4f9c62697451a6818f0a6b95af40d7d5
pembri <pembriahmad@gmail.com>
2026-05-08 12:38:23 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,19 @@
-const videoData = [];
+const videoData = [
+    {
+        "id": 1715113800000,
+        "title": "Trailer Film Action Super Keren 2026",
+        "category": "Action",
+        "slug": "/content_video/Action/trailer-film-action-super-keren-2026",
+        "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+        "date": "2026-05-08"
+    },
+    {
+        "id": 1778218699719,
+        "title": "Mucikari (2022)",
+        "category": "Kelas Bintang",
+        "slug": "/content_video/Kelas Bintang/mucikari-2022.html",
+        "thumbnail": "https://muviblast.com/wp-content/uploads/2024/03/Mucikari-2022-KB-Kelas-Bintang-152x228.jpg",
+        "embed": "https://q324op.cloudatacdn.com/u5kj2q5sw3dlsdgge6zsqlyadkka5mlr6bidawxgdsqmfqd2pkd35dpcfe5q/vzaxy1kk5d~CdrK1BGwLB?token=cl1ytcko0akkyhubsuqv3n9q&expiry=1778218684335",
+        "date": "2026-05-08"
+    }
+];
