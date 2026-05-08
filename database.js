72dec253f9b61c6a3c95c3693c62ed3ac016248d
pembri <pembriahmad@gmail.com>
2026-05-08 12:35:59 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,1 @@
-const videoData = [
-    {
-        "id": 1715113800000,
-        "title": "Trailer Film Action Super Keren 2026",
-        "category": "Action",
-        "slug": "/content_video/Action/trailer-film-action-super-keren-2026",
-        "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
-        "date": "2026-05-08"
-    }
-];
+const videoData = [];
