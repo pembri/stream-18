e921375d951ad99fd90f1180433a684954fad500
pembri <pembriahmad@gmail.com>
2026-05-11 04:48:21 +0700

    Admin Update Video/Kategori

--- database.js
+++ database.js
@@ -2,6 +2,22 @@ const streamDB = {
     "settings": {
         "domain": "stream18.musikanywhere.online"
     },
-    "categories": [],
-    "videos": []
+    "categories": [
+        {
+            "id": "indo",
+            "name": "Indo"
+        }
+    ],
+    "videos": [
+        {
+            "id": "1778449695595",
+            "title": "Selebgram Kinanda Putri Joget Tiktok",
+            "slug": "selebgram-kinanda-putri-joget-tiktok",
+            "categoryId": "indo",
+            "url": "https://stream.kingbokep.video/selebgram-kinanda-putri-joget-tiktok/playlist.m3u8",
+            "thumbnail": "https://cdn.kingbokep.video/thumbs/selebgram-kinanda-putri-joget-tiktok.webp",
+            "desc": "",
+            "dateAdded": 1778449695602
+        }
+    ]
 };
