8685ab946aad2d553cacf43a384caa7b5a242639
pembri <pembriahmad@gmail.com>
2026-05-11 05:13:58 +0700

    Admin Update Video/Kategori

--- database.js
+++ database.js
@@ -6,9 +6,23 @@ const streamDB = {
         {
             "id": "indo",
             "name": "Indo"
+        },
+        {
+            "id": "china",
+            "name": "China"
         }
     ],
     "videos": [
+        {
+            "id": "1778451233101",
+            "title": "Pijat Modus",
+            "slug": "pijat-modus",
+            "categoryId": "china",
+            "url": "https://hls.jqbsrf.cn/videos5/10f16ca79990277ecfdb3127ad47e9da/10f16ca79990277ecfdb3127ad47e9da.m3u8?auth_key=1778451145-6a0102c954180-0-06ef59c8ba894c772c3881fdcc46d7d6&v=3&time=0&via=91porna&via_bm=dx",
+            "thumbnail": "",
+            "desc": "",
+            "dateAdded": 1778451233106
+        },
         {
             "id": "1778450920703",
             "title": "Hijab Mahasiwi Unpam",
