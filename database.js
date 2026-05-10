bc6289ec4ffe003b08973db88f0f751334194c06
pembri <pembriahmad@gmail.com>
2026-05-11 05:03:34 +0700

    Admin Update Video/Kategori

--- database.js
+++ database.js
@@ -10,6 +10,16 @@ const streamDB = {
     ],
     "videos": [
         {
+            "id": "1778450610213",
+            "title": "Ukhti Jilbab Pashmina Ngewe",
+            "slug": "ukhti-jilbab-pashmina-ngewe",
+            "categoryId": "indo",
+            "url": "https://stream.kingbokep.video/ukhti-jilbab-pashmina-ngewe-part-1/playlist.m3u8",
+            "thumbnail": "https://cdn.kingbokep.video/thumbs/ukhti-jilbab-pashmina-ngewe-part-1.webp",
+            "desc": "",
+            "dateAdded": 1778450610214
+        },
+        {
             "id": "1778449695595",
             "title": "Selebgram Kinanda Putri Joget Tiktok",
             "slug": "selebgram-kinanda-putri-joget-tiktok",
