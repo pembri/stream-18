79a0f375682b371bd6d60f50f17058aa8222cfa0
pembri <pembriahmad@gmail.com>
2026-05-11 04:46:57 +0700

    Admin Update Video/Kategori

--- database.js
+++ database.js
@@ -2,21 +2,6 @@ const streamDB = {
     "settings": {
         "domain": "stream18.musikanywhere.online"
     },
-    "categories": [
-        { "id": "movies", "name": "Movies" },
-        { "id": "series", "name": "TV Series" },
-        { "id": "anime", "name": "Anime" }
-    ],
-    "videos": [
-        {
-            "id": "1715400000000",
-            "title": "Welcome to STREAM 18",
-            "slug": "welcome-to-stream-18",
-            "categoryId": "movies",
-            "url": "https://www.youtube.com/embed/aqz-KE-bpKQ",
-            "thumbnail": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?auto=format&fit=crop&w=800&q=80",
-            "desc": "Platform streaming video premium dengan performa tinggi.",
-            "dateAdded": 1715400000000
-        }
-    ]
+    "categories": [],
+    "videos": []
 };
